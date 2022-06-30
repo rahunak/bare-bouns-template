799e742f34148fa972c2580992d564e33bde3635
unknown <66bratz@gmail.com>
2022-06-30 12:43:55 +0300

    feat: add eslint for JavaScript

--- src/components/scripts/main.js
+++ src/components/scripts/main.js
@@ -1,14 +1,11 @@
+console.log("Hello world, i'm here");
 
-console.log("Hello world, i'm here")
+const dsfgdf = 445;
+const Lapa = 'Lapa';
 
-
-let a=445;
-const Lapa    ='Lapa'            ;
-
-function func (data){
-    return data*2;
+function func(data) {
+  return data * 2;
 }
 
-
-console.log('correct--->',func(a));
-console.log(func('incorrect===>',Lapa));
+console.log('correct--->', func(dsfgdf));
+console.log(func('incorrect===>', Lapa));
