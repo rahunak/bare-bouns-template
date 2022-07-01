1e2bb81f62b7488fad26a455ccb57a98ec04f6dd
unknown <66bratz@gmail.com>
2022-07-01 10:52:25 +0300

    feat: add basic for ts

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,49 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: './src/index.js',
 
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js',
+  },
+  devtool: 'source-map', // Enable sourcemaps for debugging webpack's output.
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: ['', '.webpack.js', '.web.js', '.ts', '.tsx', '.js'],
   },
   module: {
     rules: [
-      { test: /\.css$/, use: ["style-loader", "css-loader"] },
+      // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
+      { test: /\.tsx?$/, loader: 'ts-loader' },
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      { test: /\.js$/, loader: 'source-map-loader' },
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
       {
         test: /\.s[ac]ss$/i,
         use: [
           // Creates `style` nodes from JS strings
-          "style-loader",
+          'style-loader',
           // Translates CSS into CommonJS
-          "css-loader",
+          'css-loader',
           // Compiles Sass to CSS
-          "sass-loader",
+          'sass-loader',
         ],
       },
       {
         test: /\.html$/i,
-        loader: "html-loader",
+        loader: 'html-loader',
       },
-      { test: /\.(js)$/, use: "babel-loader" },
+      { test: /\.(js)$/, use: 'babel-loader' },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: "asset/resource",
+        type: 'asset/resource',
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
+        type: 'asset/resource',
       },
     ],
   },
@@ -47,18 +56,18 @@ module.exports = {
 
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "./src/components/html/index.html"), // какой файл обрабатываем?
-      filename: "index.html", // результат - название выходного файла
+      template: path.resolve(__dirname, './src/components/html/index.html'), // какой файл обрабатываем?
+      filename: 'index.html', // результат - название выходного файла
     }),
     new HtmlWebpackPlugin({
       template: path.resolve(
         __dirname,
-        "./src/components/html/secondPage.html"
+        './src/components/html/secondPage.html',
       ), // какой файл обрабатываем?
-      filename: "secondPage.html", // результат - название выходного файла
+      filename: 'secondPage.html', // результат - название выходного файла
     }),
     new CleanWebpackPlugin(),
   ],
 
-  mode: process.env.NODE_ENV === "production" ? "production" : "development",
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
 };
